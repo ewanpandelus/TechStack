0942d921d91bb2a01d9e67a56ae6d6024d238891
Ewan Pandelus <ewanpandelus@hotmail.com>
2020-07-31 13:02:34 +0100

    Initial setup of domain classes

---
+++ Techstack.Domain/Technology.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechStack.Domain
+{
+   public class Technology
+    {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public TechStack Techstack { get; set; }
+    public int TechStackId { get; set; }
+
+    }
+}
